bf5326ebb1a94a5110e157aafac77a6b65024603
Sefinek <contact@sefinek.net>
2025-04-13 16:42:29 +0200

    Update

--- services/cache.js
+++ services/cache.js
@@ -1,16 +1,28 @@
-const { existsSync, readFileSync, writeFileSync } = require('node:fs');
+const { dirname } = require('node:path');
+const { mkdirSync, existsSync, readFileSync, writeFileSync } = require('node:fs');
 const { CACHE_FILE, IP_REPORT_COOLDOWN } = require('../../config.js').MAIN;
 const log = require('../utils/log.js');
 
 const reportedIPs = new Map();
 
+const ensureCacheDir = () => {
+	const dir = dirname(CACHE_FILE);
+	if (!existsSync(dir)) {
+		mkdirSync(dir, { recursive: true });
+		log(0, `Created missing directory for cache: ${dir}`);
+	}
+};
+
 const loadReportedIPs = () => {
+	ensureCacheDir();
+
 	if (existsSync(CACHE_FILE)) {
 		readFileSync(CACHE_FILE, 'utf8')
 			.split('\n')
+			.filter(Boolean)
 			.forEach(line => {
-				const [ip, time] = line.split(' ');
-				if (ip && time) reportedIPs.set(ip, Number(time));
+				const [ip, time] = line.trim().split(/\s+/);
+				if (ip && !isNaN(time)) reportedIPs.set(ip, Number(time));
 			});
 
 		log(0, `Loaded ${reportedIPs.size} IPs from ${CACHE_FILE}`);
@@ -19,7 +31,10 @@ const loadReportedIPs = () => {
 	}
 };
 
-const saveReportedIPs = () => writeFileSync(CACHE_FILE, Array.from(reportedIPs).map(([ip, time]) => `${ip} ${time}`).join('\n'), 'utf8');
+const saveReportedIPs = () => {
+	ensureCacheDir();
+	writeFileSync(CACHE_FILE, Array.from(reportedIPs).map(([ip, time]) => `${ip} ${time}`).join('\n'), 'utf8');
+};
 
 const isIPReportedRecently = ip => {
 	const reportedTime = reportedIPs.get(ip);
