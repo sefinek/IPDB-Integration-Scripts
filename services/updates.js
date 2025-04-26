9717787f2c0f18fe7f1d369ce4ed606522f1fac3
Sefinek <contact@sefinek.net>
2025-04-27 01:34:22 +0200

    Fixes

--- services/updates.js
+++ services/updates.js
@@ -3,21 +3,19 @@ const { CronJob } = require('cron');
 const restartApp = require('./reloadApp.js');
 const log = require('../utils/log.js');
 const { AUTO_UPDATE_SCHEDULE } = require('../../config.js').MAIN;
+
 const git = simpleGit();
 
 const pull = async () => {
-	log('Pulling the repository and submodules...');
+	log('Pulling repository and submodules...', 0, true);
 
-	let mainSummary;
 	try {
-		const result = await git.pull(['--recurse-submodules']);
-		mainSummary = result.summary;
+		const { summary } = await git.pull(['--recurse-submodules']);
+		return summary;
 	} catch (err) {
 		log(err, 3);
 		return null;
 	}
-
-	return { mainSummary, submoduleChanges: false };
 };
 
 const pullAndRestart = async () => {
@@ -25,18 +23,16 @@ const pullAndRestart = async () => {
 		const result = await pull();
 		if (!result) return;
 
-		const { mainSummary, submoduleChanges } = result;
-		const { changes, insertions, deletions } = mainSummary;
+		const { changes, insertions, deletions } = result;
+		const hasUpdates = changes > 0 || insertions > 0 || deletions > 0;
 
-		const hasUpdates = (changes > 0 || insertions > 0 || deletions > 0 || submoduleChanges);
 		if (hasUpdates) {
 			log(`Main repo - Changes: ${changes}; Deletions: ${deletions}; Insertions: ${insertions}`, 1, true);
-			if (submoduleChanges) log('Detected updates in submodules', 1, true);
+			log('Updates detected, restarting app...', 1, true);
 
-			log('Updates detected (main repo or submodules), restarting app...', 1, true);
 			await restartApp();
 		} else {
-			log('Great! No new updates detected.', 1, true);
+			log('No updates detected.', 1, true);
 		}
 	} catch (err) {
 		log(err, 3);
