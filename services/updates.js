63da962181856af328d329140a9bb95cd8873329
Sefinek <contact@sefinek.net>
2025-04-27 02:26:24 +0200

    Update

--- services/updates.js
+++ services/updates.js
@@ -40,4 +40,4 @@ const pullAndRestart = async () => {
 // https://crontab.guru
 new CronJob(AUTO_UPDATE_SCHEDULE, pullAndRestart, null, true);
 
-module.exports = pull;
+module.exports = pullAndRestart;
