684b5705c461da93cfa47cd48f320b7088b2ea1e
Sefinek <contact@sefinek.net>
2025-04-28 11:35:10 +0200

    Update

--- utils/repo.js
+++ utils/repo.js
@@ -1,8 +1,8 @@
-const { version, homepage } = require('../../package.json');
+const { version, homepage, author: authorEmailWebsite } = require('../../package.json');
 
 const match = homepage.match(/github\.com\/([^\\/]+)\/([^#\\/]+)/) || [];
 const [, author, name] = match;
 const authorAndName = `${author}/${name}`;
 const repoFullUrl = homepage.split('#')[0] || '';
 
-module.exports = { version, author, name, authorAndName, repoFullUrl };
+module.exports = { version, author, authorEmailWebsite, name, authorAndName, repoFullUrl };
