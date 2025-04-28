abcf421d12ca27359f70f5aa400a2454c931cc02
Sefinek <contact@sefinek.net>
2025-04-28 10:23:18 +0200

    Update

--- services/reloadApp.js
+++ services/reloadApp.js
@@ -2,20 +2,23 @@ const { exec } = require('node:child_process');
 const ecosystem = require('../../ecosystem.config.js');
 const log = require('../utils/log.js');
 
-const executeCmd = cmd =>
-	new Promise((resolve, reject) => {
-		exec(cmd, (err, stdout, stderr) => {
-			if (err || stderr) {
-				log(`Error executing command: ${cmd}\n${stderr || err}`, 3);
-				reject(err || stderr);
-			} else {
-				resolve(stdout);
-			}
-		});
+const executeCmd = cmd => new Promise((resolve, reject) => {
+	exec(cmd, (err, stdout, stderr) => {
+		if (err) {
+			log(`Error executing command: ${cmd}\n${err}`, 3);
+			return reject(err);
+		}
+		if (stderr) log(`Warning while executing: ${cmd}\n${stderr}`, 2);
+
+		resolve(stdout.trim());
 	});
+});
 
 const CMD_1 = 'npm install --omit=dev';
-const CMD_2 = `pm2 restart ${ecosystem.apps[0].name}`;
+const CMD_2 = (() => {
+	if (!ecosystem.apps?.[0]?.name) throw new Error('Missing app name in ecosystem config');
+	return `pm2 restart ${ecosystem.apps[0].name}`;
+})();
 
 module.exports = async () => {
 	try {
@@ -26,7 +29,6 @@ module.exports = async () => {
 
 		// 2 - restart
 		log(`Running '${CMD_2}'...`);
-
 		const result2 = await executeCmd(CMD_2);
 		log(result2, 0, true);
 	} catch (err) {
