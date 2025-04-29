70cea87f8c2a132a82f692eb3bec31618b1e6555
Sefinek <contact@sefinek.net>
2025-04-29 08:58:40 +0200

    Update

--- services/version.js
+++ services/version.js
@@ -5,10 +5,13 @@ const log = require('../log.js');
 const packageUrl = `https://raw.githubusercontent.com/${authorAndName}/main/package.json`;
 
 module.exports = async () => {
+	log('Checking for new versions...');
+
 	try {
 		const { data: { version: latest } } = await axios.get(packageUrl);
-
-		if (latest !== version) log(`A new version is available! Update by running 'npm run pull' or set 'AUTO_UPDATE_ENABLED' to true and restart the process.\n> ${version} → ${latest}`, 0, true);
+		if (latest !== version) {
+			log(`A new version is available! Update by running 'npm run pull', or set 'AUTO_UPDATE_ENABLED' to true and restart the process.\n> ${version} → ${latest}`, 0, true);
+		}
 	} catch (err) {
 		log(`Failed to check version: ${err.stack}`, 3, true);
 	}
