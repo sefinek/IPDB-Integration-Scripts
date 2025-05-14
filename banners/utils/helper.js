e55f268521b99d0584e4c99ba52f0184d5ffce64
Sefinek <contact@sefinek.net>
2025-05-15 01:14:52 +0200

    Update banners

--- banners/utils/helper.js
+++ banners/utils/helper.js
@@ -5,8 +5,8 @@ const IS_PM2 = 'pm_id' in process.env;
 const color = code => IS_PM2 ? '' : `\x1b[38;2;${code}m`;
 const reset = IS_PM2 ? '' : '\x1b[0m';
 
-const headerText = text => IS_PM2 ? text : gradientText(text, 145, 55);
-const authorText = text => IS_PM2 ? text : gradientText(text, 145, 85);
+const headerText = text => IS_PM2 ? text : gradientText(text, 145, 58);
+const authorText = text => IS_PM2 ? text : gradientText(text, 145, 88);
 
 module.exports = {
 	IS_PM2,
