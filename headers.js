15e98e31428debfcd1baf571d890c94864d8ddd6
Sefinek <contact@sefinek.net>
2025-07-09 12:51:03 +0200

    Add `scripts`

--- headers.js
+++ headers.js
@@ -1,17 +1,21 @@
 'use strict';
 
-const { CLOUDFLARE_API_KEY, SNIFFCAT_API_KEY, SEFIN_API_SECRET_TOKEN } = require('../config.js').MAIN;
+const { SNIFFCAT_API_KEY, ABUSEIPDB_API_KEY, SEFIN_API_SECRET_TOKEN, CLOUDFLARE_API_KEY } = require('../config.js').MAIN;
 
-const CLOUDFLARE = {
-	headers: { 'Authorization': `Bearer ${CLOUDFLARE_API_KEY}` },
+const SNIFFCAT = {
+	headers: { 'X-Secret-Token': SNIFFCAT_API_KEY },
 };
 
 const ABUSEIPDB = {
-	headers: { 'Key': SNIFFCAT_API_KEY },
+	headers: { 'Key': ABUSEIPDB_API_KEY },
 };
 
 const SEFINEK_API = {
 	headers: { 'X-API-Key': SEFIN_API_SECRET_TOKEN },
 };
 
-module.exports = Object.freeze({ CLOUDFLARE, ABUSEIPDB, SEFINEK_API });
+const CLOUDFLARE = {
+	headers: { 'Authorization': `Bearer ${CLOUDFLARE_API_KEY}` },
+};
+
+module.exports = Object.freeze({ SNIFFCAT, ABUSEIPDB, SEFINEK_API, CLOUDFLARE });
